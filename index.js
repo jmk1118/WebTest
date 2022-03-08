690b6e3cae60176116a10ba835236b4102af78cd
MK <jmk1118@naver.com>
2022-03-08 18:33:18 +0900

    pixi 테스트

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-document.write("Java Script 설정");
+var pagewidth = window.innerWidth;
+var pageheight = window.innerHeight;
+
+var app = new PIXI.Application({width: pagewidth - 20, height: pageheight - 20, backgroundColor: 0x1099bb, transparent: false}); // PIXI 애플리케이션 추가
+document.body.appendChild(app.view); // Canvas를 DOM에 추가
+
+var container = new PIXI.Container();
+app.stage.addChild(container);
+
+var slime;
+PIXI.loader.add("./image/texture.png").add("./image/texture2.png")
+.load(()=> { // 이미지를 load한 후 불러올 콜백 함수
+    console.log('load complete');
+    
+    slime = new PIXI.Sprite(PIXI.loader.resources["./image/texture.png"].texture);
+    slime.anchor.set(0.5);
+    container.addChild(slime);
+    
+});
+
+container.position.set(pagewidth/2 - 10, pageheight/2 - 10);
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+container.interactive = true;
+container.buttonMode = true;
+
+var touch = true;
+container.on('pointertap', function(){
+    touch = !touch;
+    if(touch)
+        slime.texture = PIXI.loader.resources['./texture2.png'].texture;
+    else
+        slime.texture = PIXI.loader.resources['./texture.png'].texture;
+})
+app.ticker.add(function(delta){
+    container.rotation -= 0.01 * delta;
+})
