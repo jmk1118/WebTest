34045db8f653c882b331f7f188f9dfcd4c88e366
MK <jmk1118@naver.com>
2022-12-21 12:45:53 +0900

    fix: 헤더 수정

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ function onClickInfo() {
             cursor: ""
         },
         headers: {
-            Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1NjExODkxMTgiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzA4MTY5OCwiaWF0IjoxNjcxNTI5Njk4LCJuYmYiOjE2NzE1Mjk2OTgsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.XN-57OyBn1IkXqnnEhR3I2uL6WVsq6Csm4KTJOK2rLY"
+            'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1NjExODkxMTgiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzA4MTY5OCwiaWF0IjoxNjcxNTI5Njk4LCJuYmYiOjE2NzE1Mjk2OTgsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.XN-57OyBn1IkXqnnEhR3I2uL6WVsq6Csm4KTJOK2rLY"
         },
     })
     .done(function (msg) {
