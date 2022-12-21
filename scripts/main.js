42208bac3f28c534852ff4c03a913c0f2fb106fd
MK <jmk1118@naver.com>
2022-12-21 15:57:16 +0900

    fix: key값을 입력받을 수 있도록 수정

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,19 @@
-function onClickInfo() {
+function onClickInfo(){
+    var key = $('#key').val();
+
     $.ajax({
         method: "GET",
-        url: "https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results",
+        type: "GET",
+        url: 'https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results',
         data: {
-            count: 1000,
-            date: "2022-11-25",
-            cursor: ""
+            count: 100,
+            date: '2022-11-25',
+            cursor: ''
         },
-        headers: {
-            'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1NjExODkxMTgiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzA4MTY5OCwiaWF0IjoxNjcxNTI5Njk4LCJuYmYiOjE2NzE1Mjk2OTgsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.XN-57OyBn1IkXqnnEhR3I2uL6WVsq6Csm4KTJOK2rLY"
+        beforeSend: function(xhr){
+            xhr.setRequestHeader('Authorization', key);
         },
-    })
-    .done(function (msg) {
+    }).done(function(msg){
         console.log(msg);
     });
 }
