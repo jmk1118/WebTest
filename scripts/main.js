ed863375c8ad32e3669240e13cc98a30d23e81b6
MK <jmk1118@naver.com>
2022-12-21 11:46:22 +0900

    fix: api test

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
 function onClickInfo() {
-    if(this.infoPopup){
-        this.infoPopup.close();
-        this.infoPopup = null;
-    }
-    else{
-        this.infoPopup = window.open("Info/info.html", "Info", "width=400, height=300");
-    }
+    $.ajax({
+        method: "GET",
+        url: "https://public.api.nexon.com/openapi/maplestory/v1/cube-use-results",
+        data: {
+            count: 1000,
+            date: "2022-11-25",
+            cursor: ""
+        },
+        headers: {
+            Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1NjExODkxMTgiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4NzA4MTY5OCwiaWF0IjoxNjcxNTI5Njk4LCJuYmYiOjE2NzE1Mjk2OTgsInNlcnZpY2VfaWQiOiI0MzAwMTEzOTciLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.XN-57OyBn1IkXqnnEhR3I2uL6WVsq6Csm4KTJOK2rLY"
+        },
+    })
+    .done(function (msg) {
+        console.log(msg);
+    });
 }
